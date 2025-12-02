f1950a0caa060c790f3010729f2e118b19b4d281
Santiago Iregui <santiago.iregui@kuleuven.be>
2025-12-02 13:25:38 +0000

    Added headers to all code files and cleaned repo

--- task_specifications/follow_external_frame.etasl.lua
+++ task_specifications/follow_external_frame.etasl.lua
@@ -1,3 +1,21 @@
+--  Copyright (c) 2025 KU Leuven, Belgium
+--
+--  Author: Santiago Iregui
+--  email: <santiago.iregui@kuleuven.be>
+-- 
+--  GNU Lesser General Public License Usage
+--  Alternatively, this file may be used under the terms of the GNU Lesser
+--  General Public License version 3 as published by the Free Software
+--  Foundation and appearing in the file LICENSE.LGPLv3 included in the
+--  packaging of this file. Please review the following information to
+--  ensure the GNU Lesser General Public License version 3 requirements
+--  will be met: https://www.gnu.org/licenses/lgpl.html.
+-- 
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Lesser General Public License for more details.
+
 require("context")
 require("geometric")
 -- worldmodel=require("worldmodel")
